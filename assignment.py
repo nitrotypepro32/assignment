911dd56d0f1eb153a62cb36239803ffab2d823ac
nitrotypepro32 <pskreddy@gmail.com>
2025-03-07 20:12:57 -0600

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,13 @@
+# Define five different variables with names
+name1 = "Anika"
+name2 = "Siddharth"
+name3 = "Arshaan"
+name4 = "Amma"
+name5 = "Nana"
+
+# Print happy birthday messages
+print(f"Happy Birthday, {name1}!")
+print(f"Happy Birthday, {name2}!")
+print(f"Happy Birthday, {name3}!")
+print(f"Happy Birthday, {name4}!")
+print(f"Happy Birthday, {name5}!")
